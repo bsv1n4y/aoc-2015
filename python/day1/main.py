fa7fdc951ca3d7d0589e71e0a35fcf8c5664f641
bsv1n4y <bs.v1n4y@gmail.com>
2026-01-17 16:45:43 +0530

    Added Comments

--- python/day1/main.py
+++ python/day1/main.py
@@ -2,14 +2,15 @@ floor = 0
 
 with open("input", 'rb') as f:
     content = str(f.read())
-    array = list(content)
+    array = list(content)  # Converting file content to array or list
+    # Removing first two and last three elements from string (bytechar, newline etc...)
     array = array[2: -3]
-    array = [s.replace(',', '') for s in array]
+    array = [s.replace(',', '') for s in array]  # To remove commas from list
     for i in range(len(array)):
         if "(" in array[i]:
             floor = floor + 1
         elif ")" in array[i]:
             floor = floor - 1
         if floor == -1:
-            print(i+1)
+            print(i+1)  # Shit, array or list starts from zero, gotta add one
 print(floor)
