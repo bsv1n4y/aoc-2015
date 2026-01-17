dba1f7ebce78865f68bd04c6c8d8cc0e5d1a7b0f
bsv1n4y <bs.v1n4y@gmail.com>
2026-01-17 16:41:39 +0530

    Completed Day 1

--- python/day1/main.py
+++ python/day1/main.py
@@ -3,11 +3,13 @@ floor = 0
 with open("input", 'rb') as f:
     content = str(f.read())
     array = list(content)
+    array = array[2: -3]
+    array = [s.replace(',', '') for s in array]
     for i in range(len(array)):
         if "(" in array[i]:
             floor = floor + 1
         elif ")" in array[i]:
             floor = floor - 1
-
-
+        if floor == -1:
+            print(i+1)
 print(floor)
